7d63c53ada0b8443b8bffbc352040a25b1be1d81
JPalmaUCT <jpalma2015@alu.uct.cl>
2019-09-23 11:06:23 -0400

    Primer Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-toolbar dense fixed app>
+        <v-menu bottom left>
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on" >
+                  <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                </v-btn>
+              </template>
+  
+              <v-list>
+                <v-list-item v-for="(item, i) in items" :key="i" >
+                  <!--@click="" -->
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+        </v-menu>
+
+        <v-toolbar-title>Arquetipos</v-toolbar-title>
+        <div class="flex-grow-1"></div>
+        <v-spacer></v-spacer>
+        <v-btn icon> <v-icon>mdi-magnify</v-icon> </v-btn>
+        <v-btn icon> <v-icon>mdi-heart</v-icon> </v-btn>
+        <v-btn icon> <v-icon>mdi-dots-vertical</v-icon></v-btn>
+      </v-toolbar>
+</template>
+
+<script>
+  import '../app.js';
+    export default { 
+        data: () => ({
+      list : 0,
+      items: [
+        { title: 'Crear Arquetipo' },
+        { title: 'Editar Arquetipo' },
+        { title: 'Importar Arquetipo' },
+        { title: 'Exportar Arquetipo' },
+      ],
+    }),
+    }
+</script>
+
+<style lang="scss">
+    @import '../../../node_modules/vuetify/src/styles/main';
+</style>
+
